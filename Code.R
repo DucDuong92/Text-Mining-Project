6806c5b05549c90058a736d655dd80949d3764ab
DucDuong92 <minhducduong92@gmail.com>
2019-01-02 23:36:48 +0100

    Train and test use the same corpus

--- Code.R
+++ Code.R
@@ -21,11 +21,20 @@ id = sample(1:n, floor(n*0.8))
 train = wine[id,]
 test = wine[-id,]
 
+#small for test model
+id2 = sample(1:n, floor(n*0.1))
+wine2 <- wine[id2,]
+n2 = length(id2)
+id_test = sample(1:n2, floor(n2*0.8))
+train = wine2[id_test,]
+test = wine2[-id_test,]
+
+
 
 #Process texts
 library(dplyr)
-library(text2vec)
-library(SnowballC)
+#library(text2vec)
+#library(SnowballC)
 library(tm)
 library(stringr)
 
@@ -39,15 +48,20 @@ clean_corpus <- function(input)
     wine_corpus = tm_map(wine_corpus, stripWhitespace)
     wine_corpus <- tm_map(wine_corpus, stemDocument)
     
-    wine_dtm_tfidf <- DocumentTermMatrix(wine_corpus, control = list(weighting = weightTfIdf))
+    #wine_dtm_tfidf <- DocumentTermMatrix(wine_corpus, control = list(weighting = weightTfIdf))
+    wine_dtm_tfidf <- DocumentTermMatrix(wine_corpus)
     wine_dtm_tfidf = removeSparseTerms(wine_dtm_tfidf, 0.95)
     wine_dtm_tfidf <- as.matrix(wine_dtm_tfidf)
     
     return(wine_dtm_tfidf)
   }
 
+#create the train
+wine_dtm_tfidf <- clean_corpus(train$description)
+wine_training_set <- cbind(wine_dtm_tfidf, train$benefit)
 
-wine_input_set <- cbind(wine_input_vector, train$quality)
+#create the test
+test_dtm <- clean_corpus(test$description)
 
 ##
 colnames(wine_training_set)[ncol(wine_training_set)] <- "y"
@@ -56,32 +70,18 @@ wine_training_set <- as.data.frame(wine_training_set)
 wine_training_set$y <- as.factor(wine_training_set$y)
 
 library(caret)
-svm_model <- train(y ~., data = wine_training_set, method = 'svmLinear3')
-
-
-test_tdm <- clean_corpus(test$description)
+#train_model <- train(y ~., data = wine_training_set, method = 'svmLinear3')
+train_model <- train(y ~., data = wine_training_set, method = 'naive_bayes')
+#train_model <- train(y ~., data = wine_training_set, method = 'svmRadial')
 
 
 #Build the prediction  
-model_svm_result <- predict(svm_model, newdata = test_tdm)
+model_result <- predict(train_model, newdata = test_dtm)
 
-check_accuracy <- as.data.frame(cbind(prediction = model_svm_result, rating = test$quality))
+check_accuracy <- as.data.frame(cbind(prediction = model_result,  quality = test$benefit))
 
-check_accuracy <- check_accuracy %>% mutate(prediction = as.integer(prediction) - 1)
+check_accuracy <- check_accuracy %>% mutate(prediction = as.integer(prediction) )
 
-check_accuracy$accuracy <- if_else(check_accuracy$prediction == check_accuracy$rating, 1, 0)
+check_accuracy$accuracy <- if_else(check_accuracy$prediction == check_accuracy$quality, 1, 0)
 round(prop.table(table(check_accuracy$accuracy)), 3)
-
-
-# Old code
-# wine$description <- gsub('[[:punct:] ]+',' ',wine$description)
-# wine$description<- gsub('[[:digit:]]+', '', wine$description)
-# wine$description <- tolower(wine$description)
-# 
-# stopwords_regex = paste(stopwords('en'), collapse = '\\b|\\b')
-# stopwords_regex = paste0('\\b', stopwords_regex, '\\b')
-# wine$description = stringr::str_replace_all(wine$description, stopwords_regex, '')
-# 
-# 
-# wine$description <- str_split(wine$description, ' ')
 
