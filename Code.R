193758d726f4557cdff5c844435f6ca141ee868c
DucDuong92 <minhducduong92@gmail.com>
2019-01-02 23:52:39 +0100

    Add information for description, NB acc 79.6%

--- Code.R
+++ Code.R
@@ -10,62 +10,53 @@ wine$benefit <- wine$value > 30
 wine$benefit[wine$benefit == TRUE] <- "high"
 wine$benefit[wine$benefit == FALSE] <- "medium"
 wine$benefit <- as.factor(wine$benefit)
+wine$description <- paste(wine$description, wine$country, wine$designation, wine$province, wine$region_1, wine$region_2, wine$variety)
 wine$description <- as.character(wine$description)
 
+#Process text and create corpus
+library(dplyr)
+library(tm)
+library(stringr)
+wine_corpus = Corpus(VectorSource(wine$description))
+wine_corpus = tm_map(wine_corpus, removePunctuation)
+wine_corpus = tm_map(wine_corpus, content_transformer(tolower))
+wine_corpus = tm_map(wine_corpus, removeNumbers)
+wine_corpus = tm_map(wine_corpus, removeWords, c("the", "and", stopwords("english")))
+wine_corpus = tm_map(wine_corpus, stripWhitespace)
+wine_corpus <- tm_map(wine_corpus, stemDocument)
+
+wine_dtm_tfidf <- DocumentTermMatrix(wine_corpus, control = list(weighting = weightTfIdf))
+#wine_dtm_tfidf <- DocumentTermMatrix(wine_corpus)
+wine_dtm_tfidf <- removeSparseTerms(wine_dtm_tfidf, 0.95)
+wine_dtm_tfidf <- as.matrix(wine_dtm_tfidf)
 
 
 # split train/test
 n = dim(wine)[1]
 set.seed(12345)
-id = sample(1:n, floor(n*0.8))
-train = wine[id,]
-test = wine[-id,]
+# id = sample(1:n, floor(n*0.8))
+# train = wine[id,]
+# test = wine[-id,]
 
 #small for test model
 id2 = sample(1:n, floor(n*0.1))
+wine_sample <- wine_dtm_tfidf[id2,]
 wine2 <- wine[id2,]
 n2 = length(id2)
 id_test = sample(1:n2, floor(n2*0.8))
-train = wine2[id_test,]
-test = wine2[-id_test,]
+train = wine_sample[id_test,]
+test = wine_sample[-id_test,]
 
 
+#create the train set
+classsify <- wine2[id_test, 15]
+wine_training_set <- cbind(train, classsify)
 
-#Process texts
-library(dplyr)
-#library(text2vec)
-#library(SnowballC)
-library(tm)
-library(stringr)
-
-clean_corpus <- function(input)
-  {
-    wine_corpus = Corpus(VectorSource(input))
-    wine_corpus = tm_map(wine_corpus, removePunctuation)
-    wine_corpus = tm_map(wine_corpus, content_transformer(tolower))
-    wine_corpus = tm_map(wine_corpus, removeNumbers)
-    wine_corpus = tm_map(wine_corpus, removeWords, c("the", "and", stopwords("english")))
-    wine_corpus = tm_map(wine_corpus, stripWhitespace)
-    wine_corpus <- tm_map(wine_corpus, stemDocument)
-    
-    #wine_dtm_tfidf <- DocumentTermMatrix(wine_corpus, control = list(weighting = weightTfIdf))
-    wine_dtm_tfidf <- DocumentTermMatrix(wine_corpus)
-    wine_dtm_tfidf = removeSparseTerms(wine_dtm_tfidf, 0.95)
-    wine_dtm_tfidf <- as.matrix(wine_dtm_tfidf)
-    
-    return(wine_dtm_tfidf)
-  }
-
-#create the train
-wine_dtm_tfidf <- clean_corpus(train$description)
-wine_training_set <- cbind(wine_dtm_tfidf, train$benefit)
-
-#create the test
-test_dtm <- clean_corpus(test$description)
+#create the test result
+wine_testing_result <- wine2[-id_test, 15]
 
 ##
 colnames(wine_training_set)[ncol(wine_training_set)] <- "y"
-
 wine_training_set <- as.data.frame(wine_training_set)
 wine_training_set$y <- as.factor(wine_training_set$y)
 
@@ -76,12 +67,11 @@ train_model <- train(y ~., data = wine_training_set, method = 'naive_bayes')
 
 
 #Build the prediction  
-model_result <- predict(train_model, newdata = test_dtm)
+model_result <- predict(train_model, newdata = test)
+check_accuracy <- as.data.frame(cbind(prediction = model_result,  classify = wine_testing_result))
 
-check_accuracy <- as.data.frame(cbind(prediction = model_result,  quality = test$benefit))
+check_accuracy <- check_accuracy %>% mutate(prediction = as.integer(prediction))
 
-check_accuracy <- check_accuracy %>% mutate(prediction = as.integer(prediction) )
-
-check_accuracy$accuracy <- if_else(check_accuracy$prediction == check_accuracy$quality, 1, 0)
+check_accuracy$accuracy <- if_else(check_accuracy$prediction == check_accuracy$classify, 1, 0)
 round(prop.table(table(check_accuracy$accuracy)), 3)
 
